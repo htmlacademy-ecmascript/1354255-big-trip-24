39be63269fd38f776f80ef331eb7152237da502a
Mars <ilon.mars@yandex.ru>
2024-08-31 00:48:28 +0300

    refactor: move duplicated logic to general class

--- src/view/component-view.js
+++ src/view/component-view.js
@@ -1,9 +1,16 @@
 import { createElement } from '@/render';
 
 class ComponentView {
+  _createTemplate;
+  _name = 'ComponentView';
+
+  constructor({ props }) {
+    this.props = props;
+  }
+
   get element() {
     if (!this._element) {
-      this._element = createElement(this.getTemplate());
+      this._element = createElement(this.template);
     }
 
     return this._element;
@@ -13,7 +20,13 @@ class ComponentView {
     this._element = null;
   }
 
-  getTemplate() {}
+  get template() {
+    if (!this._createTemplate) {
+      throw new Error(`Нужно добавить темлейт для компонента ${this._name}`);
+    }
+
+    return this._createTemplate(this.props);
+  }
 }
 
 export default ComponentView;
