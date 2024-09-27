5cc3000343abfc81ccc5cbcd34ea7daa78fabeb2
Mars <ilon.mars@yandex.ru>
2024-09-27 22:47:29 +0300

    fix: time difference

--- src/utils/points.js
+++ src/utils/points.js
@@ -1,17 +1,27 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import utc from 'dayjs/plugin/utc';
 
 import { Sort } from '@/utils';
 
+dayjs.extend(duration);
 dayjs.extend(utc);
 
+const MSECONDS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
 const MINUTES_IN_HOUR = 60;
 const HOURS_IN_DAY = 24;
+
+const MSECONDS_IN_HOUR = MSECONDS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+const MSECONDS_IN_DAY = MSECONDS_IN_HOUR * HOURS_IN_DAY;
 
 const DateTimeFormat = {
   DEFAULT: 'MMM DD',
   EDIT_POINT: 'YY/MM/DD HH:mm',
-  TIME: 'HH:mm'
+  TIME: 'HH:mm',
+  DAYS_DURATION: 'DD[D] HH[H] mm[M]',
+  HOURS_DURATION: 'HH[H] mm[M]',
+  MINUTES_DURATION: 'mm[M]'
 };
 
 const now = dayjs();
@@ -33,26 +43,19 @@ const formatEditPointDate = (date) => date ? dayjs(date).format(DateTimeFormat.E
 
 const getTimeFromDate = (date) => date ? dayjs(date).format(DateTimeFormat.TIME) : '';
 
-const fillWithZero = (number) => String(number).length === 1 ? `0${number}` : `${number}`;
-
 const getTimeDifference = (dateFrom, dateTo) => {
-  const diffInMinutes = dayjs(dateTo).diff(dayjs(dateFrom), 'minute');
-  let minutesLeft = diffInMinutes;
+  const diff = dayjs(dateTo).diff(dayjs(dateFrom));
 
-  const dayDiff = Math.floor(diffInMinutes / HOURS_IN_DAY / MINUTES_IN_HOUR);
-  minutesLeft = diffInMinutes - dayDiff * HOURS_IN_DAY * MINUTES_IN_HOUR;
+  if (diff >= MSECONDS_IN_DAY) {
+    return dayjs.duration(diff).format(DateTimeFormat.DAYS_DURATION);
+  }
 
-  const hoursDiff = Math.floor(minutesLeft / MINUTES_IN_HOUR);
-  minutesLeft = hoursDiff * MINUTES_IN_HOUR;
+  if (diff >= MSECONDS_IN_HOUR) {
+    return dayjs.duration(diff).format(DateTimeFormat.HOURS_DURATION);
+  }
 
-  const minutesDiff = minutesLeft;
-
-  if (dayDiff) {
-    return `${fillWithZero(dayDiff)}D ${fillWithZero(hoursDiff)}H ${fillWithZero(minutesDiff)}M`;
-  } else if (hoursDiff) {
-    return `${fillWithZero(hoursDiff)}H ${fillWithZero(minutesDiff)}M`;
-  } else {
-    return `${fillWithZero(minutesDiff)}M`;
+  if (diff < MSECONDS_IN_HOUR) {
+    return dayjs.duration(diff).format(DateTimeFormat.MINUTES_DURATION);
   }
 };
 
